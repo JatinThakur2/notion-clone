9c82cf0a5a2a76d2ad0fe37ec59ca0496f89650b
Jatin Thakur <jatinthakur3333@gmail.com>
2024-04-18 19:07:46 +0530

    login and sign'

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -5,17 +5,43 @@ import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
 
+import { useConvexAuth } from "convex/react";
+import { SignInButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+
 export const Navbar = () => {
+  const {isAuthenticated, isLoading} = useConvexAuth();
   const scrolled = useScrollTop();
   return (
     <div
       className={cn(
-        "z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-4",
+        "z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6",
         scrolled && "border-b shadow-sm"
       )}
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-2">
+        {isLoading && (
+          <Spinner/>
+        )}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Login
+              </Button>
+            </SignInButton>
+
+            <SignInButton mode="modal">
+              <Button size="sm">
+                Get Jotion Free
+              </Button>
+            </SignInButton>
+          </>
+
+        )}
+
         <ModeToggle />
       </div>
     </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
